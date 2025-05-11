688bb3c75c62f00df360ad076e22bda3c98ed5c7
GarvitRakhechaa <garvitrakhecha786@gmail.com>
2025-05-11 13:16:25 +0530

    added arxiv loader and wikipedia loader

--- Day 8 Langchain/dataIngestion.py
+++ Day 8 Langchain/dataIngestion.py
@@ -24,4 +24,20 @@ web_documents = BaseLoader.load()  # text document
 # print(web_documents)
 
 #------------------------------------------------------------------
- 
+# arxiv loader
+
+from langchain_community.document_loaders import ArxivLoader
+arxiv_loader = ArxivLoader(query="1706.03762" )
+arxiv_documents = arxiv_loader.load()  #arxiv document
+# print(arxiv_documents)
+
+#------------------------------------------------------------------
+
+#Wikipedia loader
+from langchain_community.document_loaders import WikipediaLoader
+wiki_loader = WikipediaLoader("Langchain")
+wiki_documents = wiki_loader.load()  # wikipedia document
+# print(wiki_documents)
+
+#------------------------------------------------------------------
+
