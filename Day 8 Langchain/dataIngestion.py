fe2fe49abe812c61d2a8caf8466ece9b6a9b9ae1
GarvitRakhechaa <garvitrakhecha786@gmail.com>
2025-05-11 10:47:32 +0530

    pdf loader added

--- Day 8 Langchain/dataIngestion.py
+++ Day 8 Langchain/dataIngestion.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders import TextLoader
-from langchain_community.document_loaders import PyDFLoader
 
 loader = TextLoader("Phase 1 Foundations and Core Concep.txt")
 # print(loader)
 text_documents = loader.load()  # text document
 print(text_documents)
 
+#------------------------------------------------------------------
 
+
+from langchain_community.document_loaders import PyPDFLoader
+
+pdfloader = PyPDFLoader("tutorial.pdf")
+pdf_documents = pdfloader.load()  # text document
+print(pdf_documents)
+#------------------------------------------------------------------
