622f499228c1e964e1a96ef3b279b3942763a304
GarvitRakhechaa <garvitrakhecha786@gmail.com>
2025-05-11 10:58:31 +0530

    web base loader done

--- Day 8 Langchain/dataIngestion.py
+++ Day 8 Langchain/dataIngestion.py
@@ -1,16 +1,27 @@
+#Text Loader
 from langchain_community.document_loaders import TextLoader
 
 loader = TextLoader("Phase 1 Foundations and Core Concep.txt")
 # print(loader)
 text_documents = loader.load()  # text document
-print(text_documents)
+# print(text_documents)
 
 #------------------------------------------------------------------
-
+#pdf Loader
 
 from langchain_community.document_loaders import PyPDFLoader
 
 pdfloader = PyPDFLoader("tutorial.pdf")
 pdf_documents = pdfloader.load()  # text document
-print(pdf_documents)
+# print(pdf_documents)
 #------------------------------------------------------------------
+
+#Web based loader
+from langchain_community.document_loaders import WebBaseLoader
+
+BaseLoader = WebBaseLoader("https://docs.chaicode.com/")
+web_documents = BaseLoader.load()  # text document
+# print(web_documents)
+
+#------------------------------------------------------------------
+ 
